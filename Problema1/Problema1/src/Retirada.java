68fd80e88e63586d64f3b61d67fb636e6877cf75
Vinícius Estrázulas Mattos <124388349+vemattos@users.noreply.github.com>
2025-02-23 14:11:53 -0300

    Resolução do problema 1

---
+++ Problema1/Problema1/src/Retirada.java
@@ -0,0 +1,9 @@
+
+public class Retirada extends TipoEntrega {
+
+	@Override
+	public double calcularValorEntrega(double pesoTotal) {
+		return 0;
+	}
+	
+}
---
+++ Problema1/Problema1/src/TipoEntrega.java
@@ -0,0 +1,4 @@
+
+public abstract class TipoEntrega {
+	abstract double calcularValorEntrega(double pesoTotal);
+}
